7fd061e55aa890ea6c7d266f362ef7e9e499ee41
Ivan Azoyan <azoyan.89@mail.ru>
2023-08-21 19:57:29 +0000

    fix dir name

--- sw.js
+++ sw.js
@@ -22,8 +22,8 @@ self.addEventListener('install', e => {
         './pencil-square.svg',
         './safari-pinned-tab.svg',
         './script.js',
-        './well-known/',
-        './well-known/assetlinks.json',
+        './.well-known/',
+        './.well-known/assetlinks.json',
         './index.html',
         './manifest.json'
       ]);
