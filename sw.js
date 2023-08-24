4553a672930549898317c73700abb48772a2bb4c
Ivan Azoyan <azoyan.89@mail.ru>
2023-08-24 21:35:32 +0000

    replace sw.js to autogenerated

--- sw.js
+++ sw.js
@@ -1,55 +1,47 @@
-const CACHE_NAME = 'Tracing Paper';
+// This is the "Offline page" service worker
 
-// Cache all the files to make a PWA
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      // Our application only has two files here index.html and manifest.json
-      // but you can add more such as style.css as your app grows
-      return cache.addAll([
-        '/',
-        './android-chrome-192x192.png',
-        './apple-touch-icon.png',
-        './browserconfig.xml',
-        './favicon-16x16.png',
-        './favicon-32x32.png',
-        './favicon.ico',
-        './mstile-70x70.png',
-        './mstile-144x144.png',
-        './mstile-150x150.png',
-        './mstile-310x150.png',
-        './mstile-310x310.png',
-        './pencil-square.svg',
-        './safari-pinned-tab.svg',
-        './script.js',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
+        if (preloadResp) {
+          return preloadResp;
+        }
 
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
